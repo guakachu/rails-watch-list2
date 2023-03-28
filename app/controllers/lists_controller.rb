211834c450da8894d2688d5d632833ada60610cd
Thomas Picamoles <thomas.picamoles@gmail.com>
2023-03-28 22:58:36 +0200

    Day 1

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,39 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show destroy]
+
   def index
+    @lists = List.all
   end
 
   def show
+    @bookmark = Bookmark.new
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.create(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
